1113c35aa6d13328ee8e17078d76de07f8240eaa
AmanWorku <amexworku2014@gmail.com>
2023-02-28 18:07:24 +0300

    Add more queries as requested

--- queries.sql
+++ queries.sql
@@ -33,4 +33,11 @@ UPDATE animals SET weight_kg = weight_kg * -1;
 ROLLBACK TO SP1;
 UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
 COMMIT;
+SELECT * FROM animals;
 
+SELECT COUNT(*) AS "Number of Animals" FROM animals;
+SELECT COUNT(*) AS "Number of Animals no escape" FROM animals WHERE escape_attempts = 0;
+SELECT AVG(weight_kg) FROM animals;
+SELECT neutered, MAX(escape_attempts) FROM animals GROUP BY neutered;
+SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
+SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth >= 'Jan 1, 1990' AND date_of_birth < 'Jan 1, 2000' GROUP BY species;
