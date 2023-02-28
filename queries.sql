9f7b82349eea34c9eb328b9a4949cff580e67a63
AmanWorku <amexworku2014@gmail.com>
2023-02-28 17:16:20 +0300

    Add queries to work on savepoint

--- queries.sql
+++ queries.sql
@@ -25,3 +25,12 @@ BEGIN;
 TRUNCATE animals;
 ROLLBACK;
 SELECT * FROM animals;
+
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > 'Jan 1, 2022';
+SAVEPOINT SP1;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO SP1;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+COMMIT;
+
