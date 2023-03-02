df9cdf26d39d7a6b0bf6dc2db7f61b1ce146ef25
AmanWorku <amexworku2014@gmail.com>
2023-03-02 11:43:20 +0300

    Add tables to the database

--- schema.sql
+++ schema.sql
@@ -45,3 +45,18 @@ ALTER TABLE animals
 ALTER TABLE animals
     ADD CONSTRAINT fk_owner_id FOREIGN KEY (owner_id) REFERENCES owners(id);
 
+CREATE TABLE vets (
+id SERIAL PRIMARY KEY,
+name VARCHAR(150),
+age INT,
+date_of_graduation DATE);
+
+CREATE TABLE sepecializations (
+species_id INT REFERENCES species(id),
+vet_id INT REFERENCES vets(id),
+PRIMARY KEY(species_id,vet_id));
+
+CREATE TABLE visits (
+animal_id INT REFERENCES animals(id),
+vet_id INT REFERENCES vets(id), date DATE,
+PRIMARY KEY(animal_id,vet_id));
